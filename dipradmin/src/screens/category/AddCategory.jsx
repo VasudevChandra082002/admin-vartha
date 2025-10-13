715976292829789866a42557745ebed0cf257232
thevasudev <vasudev082002@gmail.com>
2025-10-13 14:48:08 +0530

    changes

--- dipradmin/src/screens/category/AddCategory.jsx
+++ dipradmin/src/screens/category/AddCategory.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Form, Input, Button, Card, message, Select } from "antd";
-import { addCategory } from "../../service/Category/CategoryService";
+import { addCategory } from "../../service/category/categoryservice";
 
 const { TextArea } = Input;
 const { Option } = Select;
